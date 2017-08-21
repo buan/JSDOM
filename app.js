a96c942fe10b0ee45633c3fe618f16d6ee2bb37a
Soumya Banerjee <soumyab.sers@gmail.com>
2017-08-21 16:41:55 +0530

    second commit

--- app.js
+++ app.js
@@ -4,6 +4,36 @@ list.forEach(function(items){
   console.log(items);
 });
 
-Array.from(document.querySelectorAll(".name")).forEach(function(item){
+/*Array.from(document.querySelectorAll(".name")).forEach(function(item){
     console.log(item);
+});*/
+
+/*loop through*/
+/*document.querySelectorAll(".name").forEach(function(item){
+	console.log(item);
+});*/
+
+/*get text content */
+
+/*
+document.querySelectorAll(".name").forEach(function(item){
+	console.log(item.textContent);
 });
+*/
+
+/*change text content*/
+
+/*document.querySelectorAll(".name").forEach(function(item){
+	item.textContent="test";
+});*/
+
+/*append textcontent*/
+
+/*document.querySelectorAll(".name").forEach(function(item){
+	item.textContent=item.textContent+"test";
+});*/
+
+/*change html+append html */
+const data=document.querySelector('.name')
+data.innerHTML="<h2>Soumya</h2>"
+data.innerHTML+="<h2>Suman</h2>"
