d0fc40b33341946688891f3fcbdfbebe1280722e
buan <sbxcuseme000@gmail.com>
2017-09-10 21:44:30 +0530

    add custom filter

---
+++ app8.js
@@ -0,0 +1,36 @@
+const ol=document.querySelector(".list-name");
+const btn=document.querySelector("#add1");
+btn.addEventListener('submit',function(e){
+    e.preventDefault();
+    console.log("default behaviour prevented");
+    var textvalue=document.querySelector("input[type='text']").value;
+    //console.log(textvalue);
+    
+    //create element
+    var li=document.createElement("li");
+    var span=document.createElement("span");
+    var button=document.createElement("button");
+    
+   //add class to button
+	button.classList.add("delete");
+	
+   //add text to li and delete
+    li.textContent=textvalue;
+    button.textContent="delete";
+	
+	    
+    //append ol with li and li with delete
+   li.appendChild(span);
+   li.appendChild(button);
+   ol.appendChild(li);
+    
+});
+
+
+const search=document.querySelector("#search-form input")
+search.addEventListener("keyup",function(e){
+   
+console.log(e.target.value);
+});
+
+
